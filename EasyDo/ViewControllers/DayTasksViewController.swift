8b55e2659f6b67f80d50f7219b511e14de65e9e9
MaximusTheFuture <naturemade@yandex.ru>
2022-01-06 19:13:00 +0300

    Some refactor

--- EasyDo/ViewControllers/DayTasksViewController.swift
+++ EasyDo/ViewControllers/DayTasksViewController.swift
@@ -20,14 +20,8 @@ class DayTasksViewController: UIViewController {
     lazy var isAddMyDay: Bool = false
     weak var dayTaskDelegate: IsNeedToAddDayTaskDelegate?
     let coreDataStack = CoreDataStack(modelName: "EasyDo")
-    var myDailyItems: [DailyItems] = []
-    var fetchRequest: NSFetchRequest<DailyItems>?
     var myDayLabel = UIButton()
-    var label = UILabel()
-    var whiteView = UIView()
-    
-
-    
+  
     lazy var fetchedResultsController:
     NSFetchedResultsController<DailyItems> = {
         let fetchRequest: NSFetchRequest<DailyItems> = DailyItems.fetchRequest()
@@ -41,9 +35,7 @@ class DayTasksViewController: UIViewController {
         
         return fetchedResultsController
     }()
-    
-    
-    
+
     var tableView = UITableView()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,14 +43,7 @@ class DayTasksViewController: UIViewController {
         view.addSubview(tableView)
         tableView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom:  view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 40, left: 0, bottom: 0, right: 0))
         myDayLabelInit()
-        
         navigationController?.navigationBar.isHidden = true
-        
-        
-//        whiteView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: tableView.topAnchor, trailing: view.trailingAnchor)
-//        whiteView.backgroundColor = .white
-//        whiteView.addSubview(myDayLabel)
-       
         view.backgroundColor = .white
         
         do {
@@ -67,8 +52,6 @@ class DayTasksViewController: UIViewController {
         } catch let err as NSError {
           print("cannot fetch", err)
         }
-
-//        self.tableView.register(DayTaskViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         tableView.register(DayTaskViewCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.backgroundColor = .white
         tableView.delegate = self
@@ -85,6 +68,7 @@ class DayTasksViewController: UIViewController {
         myDayLabel.setTitleColor(.black, for: .normal)
         let interaction = UIContextMenuInteraction(delegate: self)
         self.myDayLabel.addInteraction(interaction)
+        //MARK: TODO
         let menu = UIMenu(title: "", options: .destructive, children: [
             UIAction(title: "Projects")  { _ in
                 let vc = ViewController()
@@ -95,15 +79,11 @@ class DayTasksViewController: UIViewController {
             UIAction(title: "????") { _ in
             }
         ])
-//        myDayLabel.addTarget(self, action: #selector(buttonTap), for: .touchUpInside)
         myDayLabel.menu = menu
         myDayLabel.showsMenuAsPrimaryAction = true
     }
-    
-    @objc func buttonTap(_ sender: UIButton) {
-        print("HELLO BUTTON")
-    }
-    
+
+
     fileprivate func addButtonInit() {
         view.addSubview(addButton)
         addButton.anchor(top: nil, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16),size: CGSize(width: 0, height: 60))
@@ -134,70 +114,9 @@ class DayTasksViewController: UIViewController {
         }
             coreDataStack.saveContext()
     }
-
-    // MARK: UICollectionViewDataSource
-
-//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        return fetchedResultsController.sections?.count ?? 1
-//    }
-    
-    
-//    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-//        if velocity.y > 0 {
-//            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn) {
-//
-//                self.addButton.transform = CGAffineTransform(translationX: 0, y: 100)
-//
-//            }
-//        } else {
-//            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn) {
-//                self.addButton.transform = CGAffineTransform(translationX: 0, y: 0)
-//                self.addButton.isHidden = false
-//            }
-//        }
-//    }
-
-
-//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
-//    }
-//
-//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! DayTaskViewCell
-//        let items = fetchedResultsController.object(at: indexPath)
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateStyle = .none
-//        dateFormatter.timeStyle = .short
-//        dateFormatter.dateFormat = "HH:mm"
-//
-//        cell.timeLabel.text = dateFormatter.string(from: items.inTime ?? Date())
-//        //        cell.backgroundColor = .red
-//        cell.taskLabel.text = items.task?.title
-//        if indexPath.item == 0 {
-//            cell.shapeLayer.removeFromSuperlayer()
-//
-//        } else if indexPath.item == (collectionView.numberOfSections - 1) {
-//
-//        }
-//        // Configure the cell
-//
-//        return cell
-//    }
-//
-//    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        guard let item = fetchedResultsController.object(at: indexPath).task else { return }
-//        let vc = AddDetailViewController()
-//        vc.coreDataStack = coreDataStack
-//        vc.taskDetail = item
-//        print(vc.taskDetail)
-//        print("TITLE",  vc.taskDetail?.taskDescription)
-//        present(vc, animated: true)
-//    }
-
-
 }
 
-extension DayTasksViewController: UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate{
+extension DayTasksViewController: UITableViewDataSource, UITableViewDelegate {
     
 //    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
 //           if velocity.y > 0 {
@@ -215,23 +134,23 @@ extension DayTasksViewController: UITableViewDataSource, UITableViewDelegate, UI
 //       }
 
   
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let scrollViewContentHeight = scrollView.contentSize.height
-        let scrollViewHeight = scrollView.frame.height
-        
-        if scrollView.contentOffset.y < (scrollViewContentHeight - scrollViewHeight){
-            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn) {
-                self.addButton.transform = CGAffineTransform(translationX: 0, y: 0)
-                self.addButton.isHidden = false
-            }
-        } else {
-            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn) {
-                self.addButton.transform = CGAffineTransform(translationX: 0, y: 100)
-                
-            }
-        }
-    }
-    
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let scrollViewContentHeight = scrollView.contentSize.height
+//        let scrollViewHeight = scrollView.frame.height
+//
+//
+//        if scrollView.contentOffset.y < (scrollViewContentHeight - scrollViewHeight){
+//            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn) {
+//                self.addButton.transform = CGAffineTransform(translationX: 0, y: 0)
+//                self.addButton.isHidden = false
+//            }
+//        } else {
+//            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn) {
+//                self.addButton.transform = CGAffineTransform(translationX: 0, y: 100)
+//
+//            }
+//        }
+//    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
@@ -245,6 +164,17 @@ extension DayTasksViewController: UITableViewDataSource, UITableViewDelegate, UI
         fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            let fetchRequest = DailyItems.fetchRequest()
+            let item = try? coreDataStack.managedContext.fetch(fetchRequest)
+            coreDataStack.managedContext.delete(item![indexPath.row])
+            coreDataStack.saveContext()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+        }
+    }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! DayTaskViewCell
@@ -253,7 +183,6 @@ extension DayTasksViewController: UITableViewDataSource, UITableViewDelegate, UI
                 dateFormatter.dateStyle = .none
                 dateFormatter.timeStyle = .short
                 dateFormatter.dateFormat = "HH:mm"
-        
                 cell.timeLabel.text = dateFormatter.string(from: items.inTime ?? Date())
                 //        cell.backgroundColor = .red
                 cell.taskLabel.text = items.task?.title
@@ -263,31 +192,17 @@ extension DayTasksViewController: UITableViewDataSource, UITableViewDelegate, UI
                 } else if indexPath.item == (tableView.numberOfSections - 1) {
         
                 }
-                // Configure the cell
-        
                 return cell
     }
-    
-    
-    
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let item = fetchedResultsController.object(at: indexPath).task else { return }
                let vc = AddDetailViewController()
                vc.coreDataStack = coreDataStack
                vc.taskDetail = item
-               print(vc.taskDetail)
-               print("TITLE",  vc.taskDetail?.taskDescription)
                present(vc, animated: true)
     }
 }
-
-//extension DayTasksViewController: UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: view.frame.width, height: 100)
-//    }
-//
-//}
 
 extension DayTasksViewController: UIContextMenuInteractionDelegate {
     
@@ -307,14 +222,14 @@ func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurati
     
 }
     
-//    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, previewForHighlightingMenuWithConfiguration configuration: UIContextMenuConfiguration) -> UITargetedPreview? {
-//        let parameters = UIPreviewParameters()
-//        parameters.backgroundColor = UIColor.clear
-//        let visibleRect = myDayLabel.bounds.insetBy(dx: -10, dy: -10)
-//        let visiblePath = UIBezierPath(roundedRect: visibleRect, cornerRadius: 10.0)
-//        parameters.visiblePath = visiblePath
-//        return UITargetedPreview(view: myDayLabel, parameters: parameters)
-//    }
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, previewForHighlightingMenuWithConfiguration configuration: UIContextMenuConfiguration) -> UITargetedPreview? {
+        let parameters = UIPreviewParameters()
+        parameters.backgroundColor = UIColor.clear
+        let visibleRect = myDayLabel.bounds.insetBy(dx: -10, dy: -10)
+        let visiblePath = UIBezierPath(roundedRect: visibleRect, cornerRadius: 10.0)
+        parameters.visiblePath = visiblePath
+        return UITargetedPreview(view: myDayLabel, parameters: parameters)
+    }
     
 }
 
